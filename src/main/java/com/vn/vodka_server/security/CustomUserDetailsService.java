56e254bd7217f7fe7ab5bc39d6fccaeec1ea63d4
HauPhan <phuchau.nam2005@gmail.com>
2026-03-23 14:25:15 +0700

    fix: password empty google

--- src/main/java/com/vn/vodka_server/security/CustomUserDetailsService.java
+++ src/main/java/com/vn/vodka_server/security/CustomUserDetailsService.java
@@ -27,7 +27,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
-                user.getPassword(),
+                user.getPassword() != null ? user.getPassword() : "{noop}N/A",
                 isActive,   // enabled - false nếu INACTIVE → tự động từ chối
                 true,       // accountNonExpired
                 true,       // credentialsNonExpired
